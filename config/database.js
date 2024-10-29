68a94333af236513e212cab852baa17a2354101a
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-10-29 19:37:13 +0300

    "Updated database connection log message and added 'build' script to package.json"

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-
+const mysql2 = require('mysql2')
 const DB = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(DB,{
@@ -7,14 +7,5 @@ const sequelize = new Sequelize(DB,{
     logging: false, 
   })
 
-  async function testConnection() {
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  }
   
-  testConnection();
 module.exports = sequelize;
