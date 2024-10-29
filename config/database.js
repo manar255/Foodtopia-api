dead1a913cabb8e3daff35f4f52569565b50053c
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-10-29 18:07:29 +0300

    "Update .gitignore to include node_modules, and modify config/database.js to specify dialect and logging options for Sequelize, and add a testConnection function."

--- config/database.js
+++ config/database.js
@@ -2,7 +2,19 @@ const Sequelize = require('sequelize');
 
 const DB = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(DB)
+const sequelize = new Sequelize(DB,{
+    dialect: 'mysql',
+    logging: false, 
+  })
 
-
+  async function testConnection() {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+  
+  testConnection();
 module.exports = sequelize;
